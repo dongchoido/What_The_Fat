b51597b532fef6772216fb1137f9aca51f1e0933
dongchoido <dong7cnc@gmail.com>
2025-04-14 22:36:58 +0700

    á

--- Assets/Scripts/Objects/MonsterHealth.cs
+++ Assets/Scripts/Objects/MonsterHealth.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class MonsterHealth : MonoBehaviour
 {
+    [Header("HP")]
     public int maxHealth = 3;
     public int currentHealth;
 
+    [Header("Sprites")]
+    public Sprite strong;
+    public Sprite normal;
+    public Sprite weak;
+    private SpriteRenderer sprite;
     void Start()
     {
         currentHealth = maxHealth;
+        sprite = GetComponent<SpriteRenderer>();
+        UpdateSprite();
     }
 
     public void TakeDamage(int damage)
@@ -17,11 +25,29 @@ public class MonsterHealth : MonoBehaviour
         {
             Die();
         }
+        UpdateSprite();
     }
 
     void Die()
     {
         // Xử lý khi quái vật chết
         Destroy(gameObject);
+    }
+
+    void UpdateSprite()
+    {
+        float healthPercent = (float)currentHealth/maxHealth;
+        if (healthPercent > 0.75f)
+        {
+            sprite.sprite = strong;
+        }
+        else if (healthPercent > 0.45f)
+        {
+            sprite.sprite = normal;
+        }
+        else
+        {
+            sprite.sprite = weak;
+        }
     }
 }
