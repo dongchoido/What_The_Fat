b00a4bfedee96358827e00850cd83abb26d678ee
dongchoido <dong7cnc@gmail.com>
2025-04-14 22:41:07 +0700

    Revert "á"
    This reverts commit b51597b532fef6772216fb1137f9aca51f1e0933.

--- Assets/Scripts/Objects/MonsterHealth.cs
+++ Assets/Scripts/Objects/MonsterHealth.cs
@@ -2,20 +2,12 @@ using UnityEngine;
 
 public class MonsterHealth : MonoBehaviour
 {
-    [Header("HP")]
     public int maxHealth = 3;
     public int currentHealth;
 
-    [Header("Sprites")]
-    public Sprite strong;
-    public Sprite normal;
-    public Sprite weak;
-    private SpriteRenderer sprite;
     void Start()
     {
         currentHealth = maxHealth;
-        sprite = GetComponent<SpriteRenderer>();
-        UpdateSprite();
     }
 
     public void TakeDamage(int damage)
@@ -25,29 +17,11 @@ public class MonsterHealth : MonoBehaviour
         {
             Die();
         }
-        UpdateSprite();
     }
 
     void Die()
     {
         // Xử lý khi quái vật chết
         Destroy(gameObject);
-    }
-
-    void UpdateSprite()
-    {
-        float healthPercent = (float)currentHealth/maxHealth;
-        if (healthPercent > 0.75f)
-        {
-            sprite.sprite = strong;
-        }
-        else if (healthPercent > 0.45f)
-        {
-            sprite.sprite = normal;
-        }
-        else
-        {
-            sprite.sprite = weak;
-        }
     }
 }
