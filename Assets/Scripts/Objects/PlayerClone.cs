b4de0ed313633afb3d9a23ee1703bf73fd4cd707
dongchoido <dong7cnc@gmail.com>
2025-04-18 16:45:48 +0700

    rth

--- Assets/Scripts/Objects/PlayerClone.cs
+++ Assets/Scripts/Objects/PlayerClone.cs
@@ -4,6 +4,10 @@ public class PlayerClone : MonoBehaviour
 {
     private bool triggered = false;
 
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3 (0,5,0));
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (triggered) return;
